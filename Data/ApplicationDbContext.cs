70c41c591739a2eb84de3581e666bbbbad49e761
Dimantha Thihara Walallawita <87009586+DimanthaWalallawita@users.noreply.github.com>
2024-12-21 18:52:53 +0530

    First Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NewProject.Models;
+
+namespace NewProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<NewProject.Models.Joke> Joke { get; set; } = default!;
+    }
+}
